05b6a2b082c04033beb79f9f632c0ee5155da588
Alpen-Webdesign <dev@alpen-webdesign.at>
2026-02-13 22:40:39 +0100

    Add initial migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260213_213921 from './20260213_213921';
+import * as migration_20260213_214029 from './20260213_214029';
 
 export const migrations = [
   {
     up: migration_20260213_213921.up,
     down: migration_20260213_213921.down,
-    name: '20260213_213921'
+    name: '20260213_213921',
+  },
+  {
+    up: migration_20260213_214029.up,
+    down: migration_20260213_214029.down,
+    name: '20260213_214029'
   },
 ];
